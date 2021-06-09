a538311e93b9eb73096fd6215fca8643ddef8da8
wilsonkinyua <wilsonkinyuam@gmail.com>
2021-06-09 15:26:58 +0300

    modified look good

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -189,81 +189,27 @@ Welcome to
             <div class="heading-line"></div>
         </div>
         <div class="row">
-            <div class="col-lg-4 col-md-6 sm-padding">
-                <div class="price_wrap">
-                    <h3>Hair Styling</h3>
-                    <ul class="price_list">
-                        <li>
-                            <h4>Hair Cut</h4>
-                            <p>Barber is a person whose occupation is mainly to cut dress groom style and shave men.
-                            </p>
-                            <span class="price">$8</span>
-                        </li>
-                        <li>
-                            <h4>Hair Styling</h4>
-                            <p>Barber is a person whose occupation is mainly to cut dress groom style and shave men.
-                            </p>
-                            <span class="price">$9</span>
-                        </li>
-                        <li>
-                            <h4>Hair Triming</h4>
-                            <p>Barber is a person whose occupation is mainly to cut dress groom style and shave men.
-                            </p>
-                            <span class="price">$10</span>
-                        </li>
-                    </ul>
+
+
+                <div class="col-lg-12 col-md-12 sm-padding">
+                    <div class="price_wrap">
+                        <ul class="price_list">
+                            @foreach ($all_services as $service)
+                                <li>
+                                    <h4>{{ $service->title }}</h4>
+                                    <p>
+                                        {{ $service->description }}
+                                    </p>
+                                    <span class="price">
+                                        Ksh {{ $service->price }}
+                                    </span>
+                                </li>
+                            @endforeach
+                        </ul>
+                    </div>
                 </div>
-            </div>
-            <div class="col-lg-4 col-md-6 sm-padding">
-                <div class="price_wrap">
-                    <h3>Shaving</h3>
-                    <ul class="price_list">
-                        <li>
-                            <h4>Clean Shaving</h4>
-                            <p>Barber is a person whose occupation is mainly to cut dress groom style and shave men.
-                            </p>
-                            <span class="price">$8</span>
-                        </li>
-                        <li>
-                            <h4>Beard Triming</h4>
-                            <p>Barber is a person whose occupation is mainly to cut dress groom style and shave men.
-                            </p>
-                            <span class="price">$9</span>
-                        </li>
-                        <li>
-                            <h4>Smooth Shave</h4>
-                            <p>Barber is a person whose occupation is mainly to cut dress groom style and shave men.
-                            </p>
-                            <span class="price">$10</span>
-                        </li>
-                    </ul>
-                </div>
-            </div>
-            <div class="col-lg-4 col-md-12 sm-padding">
-                <div class="price_wrap">
-                    <h3>Face Masking</h3>
-                    <ul class="price_list">
-                        <li>
-                            <h4>White Facial</h4>
-                            <p>Barber is a person whose occupation is mainly to cut dress groom style and shave men.
-                            </p>
-                            <span class="price">$8</span>
-                        </li>
-                        <li>
-                            <h4>Face Cleaning</h4>
-                            <p>Barber is a person whose occupation is mainly to cut dress groom style and shave men.
-                            </p>
-                            <span class="price">$9</span>
-                        </li>
-                        <li>
-                            <h4>Bright Tuning</h4>
-                            <p>Barber is a person whose occupation is mainly to cut dress groom style and shave men.
-                            </p>
-                            <span class="price">$10</span>
-                        </li>
-                    </ul>
-                </div>
-            </div>
+
+
         </div>
     </div>
 </section>
@@ -275,7 +221,7 @@ Welcome to
                     <h2>Making You Look Good <br> Is In Our Haritage.</h2>
                     <p>Barber is a person whose occupation is mainly to cut dress groom <br>style and shave men's
                         and boys hair.</p>
-                    <a href="#" class="default_btn">Make Appointment</a>
+                    <a href="/" class="default_btn">Make Appointment</a>
                 </div>
             </div>
         </div>
